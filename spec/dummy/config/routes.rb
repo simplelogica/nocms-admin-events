784cce490a5c43e270554587558cdbaaffec84a1
David J. Brenes <david.brenes@simplelogica.net>
2014-06-25 16:32:11 +0200

    We mount all the engines in the routes file

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
+  mount NoCms::Admin::Engine => "/admin"
   mount NoCms::Admin::Events::Engine => "/no_cms/admin/events"
+  mount NoCms::Events::Engine => "/events"
 end
